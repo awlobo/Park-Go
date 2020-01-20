17448a52b270bfe3fa44ba90b3f5703ad6aec250
Andrea Lobo <andrea.lobo93@gmail.com>
2020-01-20 23:55:21 +0100

    Añadir implementación Google Maps

--- park_and_go/app/src/main/java/com/park_and_go/MainActivity.java
+++ park_and_go/app/src/main/java/com/park_and_go/MainActivity.java
@@ -1,5 +1,11 @@
 package com.park_and_go;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
 
     MapView map = null;
