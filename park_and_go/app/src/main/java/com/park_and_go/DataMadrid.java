5b2689a1a627370245c56215cba31af1b56446b8
Ruben Ortega <ortegamontero.ruben@gmail.com>
2020-02-03 19:51:55 +0100

    add new changes in PlacesResponse

---
+++ park_and_go/app/src/main/java/com/park_and_go/DataMadrid.java
@@ -0,0 +1,20 @@
+package com.park_and_go;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface DataMadrid {
+    @Headers("Accept: application/geo+json")
+    @GET("/egob/catalogo/202625-0-aparcamientos-publicos.json")
+    Call<PlacesResponse> getPlaces(@Query("latitud") double latitude,
+                                   @Query("longitud") double longitude,
+                                   @Query("distancia") int distance);
+
+    @Headers("Accept: application/geo+json")
+    @GET("/egob/catalogo/201000-0-embajadas-consulados.json")
+    Call<PlacesResponse> getConsulates(@Query("latitud") double latitude,
+                                       @Query("longitud") double longitude,
+                                       @Query("distancia") int distance);
+}
